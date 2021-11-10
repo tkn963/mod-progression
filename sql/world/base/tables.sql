001613c4577713d49dbd2eb203e04ba9ac9b5157
Revision <tkn963@gmail.com>
2021-11-10 15:40:20 +0100

    Columns
    Moved the columns of npc_trainer.

--- sql/world/base/tables.sql
+++ sql/world/base/tables.sql
@@ -3821,12 +3821,12 @@ DROP TABLE IF EXISTS `progression_npc_trainer`;
 CREATE TABLE `progression_npc_trainer` (
 	`ID` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0',
 	`SpellID` MEDIUMINT(9) NOT NULL DEFAULT '0',
+    `min_patch` INT(2) UNSIGNED NOT NULL DEFAULT '0',
+    `max_patch` INT(2) UNSIGNED NOT NULL DEFAULT '20',
 	`MoneyCost` INT(10) UNSIGNED NOT NULL DEFAULT '0',
 	`ReqSkillLine` SMALLINT(5) UNSIGNED NOT NULL DEFAULT '0',
 	`ReqSkillRank` SMALLINT(5) UNSIGNED NOT NULL DEFAULT '0',
 	`ReqLevel` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
-    `min_patch` INT(2) UNSIGNED NOT NULL DEFAULT '0',
-    `max_patch` INT(2) UNSIGNED NOT NULL DEFAULT '20',
 	PRIMARY KEY (`ID`, `SpellID`, `min_patch`, `max_patch`) USING BTREE
 )
 COLLATE='utf8mb4_general_ci'
