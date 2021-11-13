8e4d78b8f98e9260f8636a9d2fa56e145430d396
Revision <tkn963@gmail.com>
2021-11-13 22:15:23 +0100

    Quest templates
    Due to protected variables, I can't seem to find a way of changing the values of the templates. I'm adding this only to be safe, when or if such ability is added it can be used.

--- src/ProgressionQuest.cpp
+++ src/ProgressionQuest.cpp
@@ -17,6 +17,8 @@ class ProgressionQuest : public WorldScript
             AddGameObjectQuestEnder();
             RemoveGameObjectQuestStarter();
             AddGameObjectQuestStarter();
+
+            UpdateQuestTemplates();
         }
 
     private:
@@ -271,6 +273,42 @@ class ProgressionQuest : public WorldScript
                         if (sObjectMgr->GetGameObjectTemplate(id)->type == GAMEOBJECT_TYPE_QUESTGIVER)
                             if (sObjectMgr->GetQuestTemplate(quest))
                                 questStarterMap->insert(QuestRelations::value_type(id, quest));
+            } while (result->NextRow());
+        }
+
+        void UpdateQuestTemplates()
+        {
+            QueryResult result = WorldDatabase.PQuery("SELECT `ID`, `QuestType`, `QuestLevel`, `MinLevel`, `QuestSortID`, `QuestInfoID`, `SuggestedGroupNum`, "
+                                                      "`RequiredFactionId1`, `RequiredFactionId2`, `RequiredFactionValue1`, `RequiredFactionValue2`, `RewardNextQuest`, "
+                                                      "`RewardXPDifficulty`, `RewardMoney`, `RewardMoneyDifficulty`, `RewardBonusMoney`, `RewardDisplaySpell`, `RewardSpell`, "
+                                                      "`RewardHonor`, `RewardKillHonor`, `StartItem`, `Flags`, `RequiredPlayerKills`, `RewardItem1`, `RewardAmount1`, "
+                                                      "`RewardItem2`, `RewardAmount2`, `RewardItem3`, `RewardAmount3`, `RewardItem4`, `RewardAmount4`, `ItemDrop1`, "
+                                                      "`ItemDropQuantity1`, `ItemDrop2`, `ItemDropQuantity2`, `ItemDrop3`, `ItemDropQuantity3`, `ItemDrop4`, `ItemDropQuantity4`, "
+                                                      "`RewardChoiceItemID1`, `RewardChoiceItemQuantity1`, `RewardChoiceItemID2`, `RewardChoiceItemQuantity2`, `RewardChoiceItemID3`, "
+                                                      "`RewardChoiceItemQuantity3`, `RewardChoiceItemID4`, `RewardChoiceItemQuantity4`, `RewardChoiceItemID5`, `RewardChoiceItemQuantity5`, "
+                                                      "`RewardChoiceItemID6`, `RewardChoiceItemQuantity6`, `POIContinent`, `POIx`, `POIy`, `POIPriority`, `RewardTitle`, `RewardTalents`, "
+                                                      "`RewardArenaPoints`, `RewardFactionID1`, `RewardFactionValue1`, `RewardFactionOverride1`, `RewardFactionID2`, `RewardFactionValue2`, "
+                                                      "`RewardFactionOverride2`, `RewardFactionID3`, `RewardFactionValue3`, `RewardFactionOverride3`, `RewardFactionID4`, "
+                                                      "`RewardFactionValue4`, `RewardFactionOverride4`, `RewardFactionID5`, `RewardFactionValue5`, `RewardFactionOverride5`, "
+                                                      "`TimeAllowed`, `AllowableRaces`, `LogTitle`, `LogDescription`, `QuestDescription`, `AreaDescription`, `QuestCompletionLog`, "
+                                                      "`RequiredNpcOrGo1`, `RequiredNpcOrGo2`, `RequiredNpcOrGo3`, `RequiredNpcOrGo4`, `RequiredNpcOrGoCount1`, `RequiredNpcOrGoCount2`, "
+                                                      "`RequiredNpcOrGoCount3`, `RequiredNpcOrGoCount4`, `RequiredItemId1`, `RequiredItemId2`, `RequiredItemId3`, `RequiredItemId4`, "
+                                                      "`RequiredItemId5`, `RequiredItemId6`, `RequiredItemCount1`, `RequiredItemCount2`, `RequiredItemCount3`, `RequiredItemCount4`, "
+                                                      "`RequiredItemCount5`, `RequiredItemCount6`, `Unknown0`, `ObjectiveText1`, `ObjectiveText2`, `ObjectiveText3`, `ObjectiveText4` "
+                                                      "FROM `progression_quest_template` WHERE %u BETWEEN `min_patch` AND `max_patch`", progression->getPatchId());
+
+            if (!result)
+                return;
+
+            do
+            {
+                Field* fields = result->Fetch();
+                uint32 ID     = fields[0].GetUInt32();
+
+                Quest* quest = sObjectMgr->GetQuestTemplates().at(ID);
+
+                if (!quest)
+                    continue;
             } while (result->NextRow());
         }
 };
