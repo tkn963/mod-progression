b2c44dcc57443da81139d8aa2306c8fd4cfbad86
Revision <tkn963@gmail.com>
2021-11-13 16:39:59 +0100

    Minimized
    Reduced the amount of queries for testing.

--- sql/world/base/data/for_testing_purposes_only.sql
+++ sql/world/base/data/for_testing_purposes_only.sql
@@ -1,23 +1,23 @@
 -- Test creature spawns by setting all spawns to max patch (requires patch < 20 in config)
-INSERT INTO `progression_creature` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`)
-SELECT `guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs` FROM creature;
-
-UPDATE `progression_creature` SET `min_patch`=20;
+INSERT INTO `progression_creature` (`guid`, `min_patch`, `max_patch`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`)
+SELECT `guid`, 20, 20, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs` FROM creature;
 
 -- Test object spawns by setting all spawns to max patch (requires patch < 20 in config)
-INSERT INTO `progression_gameobject` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`)
-SELECT `guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs` FROM gameobject;
-
-UPDATE `progression_gameobject` SET `min_patch`=20;
+INSERT INTO `progression_gameobject` (`guid`, `min_patch`, `max_patch`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`)
+SELECT `guid`, 20, 20, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs` FROM gameobject;
 
 -- Test quest ender NPCs by setting all to max patch (requires patch < 20 in config)
-INSERT INTO `progression_creature_questender` (`id`, `quest`)
-SELECT `id`, `quest` FROM `creature_questender`;
-
-UPDATE `progression_creature_questender` SET `min_patch`=20;
+INSERT INTO `progression_creature_questender` (`id`, `quest`, `min_patch`, `max_patch`)
+SELECT `id`, `quest`, 20, 20 FROM `creature_questender`;
 
 -- Test quest starter NPCs by setting all to max patch (requires patch < 20 in config)
-INSERT INTO `progression_creature_queststarter` (`id`, `quest`)
-SELECT `id`, `quest` FROM `creature_queststarter`;
+INSERT INTO `progression_creature_queststarter` (`id`, `quest`, `min_patch`, `max_patch`)
+SELECT `id`, `quest`, 20, 20 FROM `creature_queststarter`;
 
-UPDATE `progression_creature_queststarter` SET `min_patch`=20;
+-- Test quest ender objects by setting all to max patch (requires patch < 20 in config)
+INSERT INTO `progression_gameobject_questender` (`id`, `quest`, `min_patch`, `max_patch`)
+SELECT `id`, `quest`, 20, 20 FROM `gameobject_questender`;
+
+-- Test quest starter objects by setting all to max patch (requires patch < 20 in config)
+INSERT INTO `progression_gameobject_queststarter` (`id`, `quest`, `min_patch`, `max_patch`)
+SELECT `id`, `quest`, 20, 20 FROM `gameobject_queststarter`;
